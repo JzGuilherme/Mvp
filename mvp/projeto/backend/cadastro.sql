967576473863bb01bb9c80f1d66815c4cb9beadf
vasconcelosfelipe642-lang <vasconcelosfelipe642@gmail.com>
2025-11-04 12:52:36 -0300

    bancoagendasql

--- mvp/projeto/backend/cadastro.sql
+++ mvp/projeto/backend/cadastro.sql
@@ -1,10 +1,8 @@
--- Primeiro, cria o banco de dados (se ele ainda não existir)
+
 CREATE DATABASE IF NOT EXISTS manup_db;
 
--- Segundo, diz ao MySQL para USAR esse banco daqui para frente
 USE manup_db;
 
--- Terceiro, cria sua tabela DENTRO do banco manup_db
 CREATE TABLE usuarios (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(150) NOT NULL,
