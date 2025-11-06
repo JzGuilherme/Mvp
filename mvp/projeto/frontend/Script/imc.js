8ae54d1cc737085993ba34183a854f59a9f60820
yuri cruz <yuricruz063@gmail.com>
2025-11-06 11:27:53 -0300

    node_modules - (develop)

--- mvp/projeto/frontend/Script/imc.js
+++ mvp/projeto/frontend/Script/imc.js
@@ -8,7 +8,7 @@ const imcMessageElement = document.getElementById('imc-message');
 function classify(imc) {
     if (imc <= 18.5) return { status: 'Abaixo do peso', message : 'Você está abaixo do peso ideal. Considere consultar um nutricionista para orientações.'}; 
     if (imc <= 24.9) return { status: 'Peso normal', message : 'Parabéns! Você está com o peso ideal. Mantenha hábitos saudáveis para continuar assim.'};
-    if (imc > 29.9 ) return { status: 'Sobrepeso', message : 'Você está com sobrepeso. Avalie sua alimentação e pratique atividades físicas regularmente.'};
+    if (imc < 29.9 ) return { status: 'Sobrepeso', message : 'Você está com sobrepeso. Avalie sua alimentação e pratique atividades físicas regularmente.'};
     else {return { status: 'Obesidade', message: 'Você está com Obesidade.' }
 }}
 
